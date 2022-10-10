ec615f633f14a2c113f752777d5f5dfe06764069
vladwithcode <vladwithcode@gmail.com>
2022-10-10 17:42:20 -0600

    [Init] First commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import '../styles/main.css';
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
