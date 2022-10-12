fb65383092c04e82ca8f0c0e4218a574fce47222
vladwithcode <vladwithcode@gmail.com>
2022-10-12 12:27:58 -0600

    [Change] Improve button styles

--- src/components/globals/Button.jsx
+++ src/components/globals/Button.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 
-const classNames = {
-  primary: 'text-white bg-primary border-primary hover:text-primary',
-  secondary: 'text-white bg-secondary border-secondary hover:text-secondary',
-  light: '',
-  dark: 'text-white bg-gray-800 border-gray-800 hover:text-gray-800',
-};
-
-function Button({ children, onClick, type, className, style = 'primary' }) {
-  const _className =
-    'border-2 p-2 rounded-md hover:bg-opacity-40 text-sm grow-0 shrink-0';
+function Button({ children, onClick, type, className }) {
+  const _className = 'btn';
 
   return (
     <button
       type={type || 'button'}
-      className={_className.concat(
-        className ? ' ' + className : ' ' + classNames[style]
-      )}
+      className={_className.concat(className ? ' ' + className : '')}
       onClick={onClick}>
       {children}
     </button>
