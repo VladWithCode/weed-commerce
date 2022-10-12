e23c423fe743ce2c04e28044a276f946fa9d157d
vladwithcode <vladwithcode@gmail.com>
2022-10-12 12:28:19 -0600

    [Change] Improve error page styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,8 +4,8 @@ import Search from '../components/Search/Search';
 
 function Error404({}) {
   return (
-    <div className='w-full h-[calc(100vh-96px)] bg-secondary'>
-      <div className='container font-secondary font-bold text-white text-center mx-auto py-14 px-2'>
+    <div className='w-full h-[calc(100vh-96px)] bg-white'>
+      <div className='container font-secondary font-bold text-gray-900 text-center mx-auto py-14 px-2'>
         <h1 className='text-8xl'>Ups...</h1>
         <p className='text-xl py-6 font-light'>
           Parece que la página que estas buscando fue eliminada o no existe.
@@ -16,7 +16,7 @@ function Error404({}) {
         <div className='min-w-min h-12 px-4'>
           <Search />
         </div>
-        <div className='flex justify-evenly mt-12 px-4 gap-x-2'>
+        <div className='flex justify-evenly mt-12 px-4 gap-x-2 text-white'>
           <Button style='primary'>Volver al inicio</Button>
           <Button style='dark'>Ir a un producto random</Button>
         </div>
