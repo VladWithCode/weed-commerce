9bb1b113432592fc172e1dc2e6e1d5f9d314440f
vladwithcode <vladwithcode@gmail.com>
2022-10-12 12:27:39 -0600

    [Change] Improve search componentt styles

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 function Search({}) {
   return (
-    <form className='border-2 border-primary w-full h-full flex rounded-lg text-primary'>
+    <form className='border-2 border-black w-full h-full flex rounded-lg text-black'>
       <input
-        className='outline-none h-full grow px-4 bg-primary bg-opacity-30 focus:bg-opacity-50 placeholder-aux_2'
+        className='outline-none h-full grow px-4 bg-white bg-opacity-30 focus:bg-opacity-50 placeholder-gray-300'
         type='text'
         name='search'
         id='search_input'
         placeholder='Busca algo como "Blue Haze"'
       />
-      <button
-        type='submit'
-        className='w-11 h-full flex bg-primary shrink-0 z-10'>
+      <button type='submit' className='w-11 h-full flex bg-black shrink-0 z-10'>
         <svg className='m-auto w-10 h-10 fill-white'>
           <use href='/svg/sprites.svg#search'></use>
         </svg>
