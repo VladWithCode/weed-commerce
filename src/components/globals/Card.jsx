fafadeec07a74a91726010984f0dce9ab2b1b77f
vladwithcode <vladwithcode@gmail.com>
2022-10-13 17:54:06 -0600

    [Add] Global Card

---
+++ src/components/globals/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Card({ className: csName, children }) {
+  const className =
+    'flex flex-col rounded-sm text-white bg-zinc-900 shrink-0 basis-80 overflow-hidden';
+
+  return (
+    <div className={csName?.length > 0 ? className + ' ' + csName : className}>
+      {children}
+    </div>
+  );
+}
+
+export default Card;
