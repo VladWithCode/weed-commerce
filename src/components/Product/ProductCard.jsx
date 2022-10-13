141e63c5b6408eec57a4c8a262ac83107d1cd64e
vladwithcode <vladwithcode@gmail.com>
2022-10-13 17:54:16 -0600

    [Add] Product Cardd

---
+++ src/components/Product/ProductCard.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+import { priceToString } from '../../utils/numberToString';
+import Card from '../globals/Card';
+
+function ProductCard({ product, className: csName }) {
+  const className = 'grow-0 shadow-md shadow-gray-300';
+
+  return (
+    <Card className={csName?.length > 0 ? className + ' ' + csName : className}>
+      <Image src='/img/weed.jpeg' width={200} height={400} />
+      <div className='flex justify-between items-center basis-11/12 p-2'>
+        <h1 className='text-xl font-semibold uppercase'>{product.name}</h1>
+        <p className='text-2xl font-light'>
+          $ {priceToString(product.price)}MXN
+        </p>
+      </div>
+      <div className='p-2'>
+        <button className='btn w-full font-bold'>Añadir al carrito</button>
+      </div>
+      <div className='p-2'>
+        <button className='btn btn-white  w-full font-bold'>
+          Comprar ahora
+        </button>
+      </div>
+    </Card>
+  );
+}
+
+export default ProductCard;
