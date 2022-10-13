1bdc6f970bc286207e515da33d8b2df15f26c21d
vladwithcode <vladwithcode@gmail.com>
2022-10-13 17:56:54 -0600

    [Add] Product gallery

---
+++ src/components/Product/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+
+function Gallery({ products }) {
+  return (
+    <div className='w-full flex flex-row items-center p-4 overflow-scroll gap-x-4'>
+      {products?.map(product => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
