7802789203da812d423a9dbcca041b14b311d4ed
vladwithcode <vladwithcode@gmail.com>
2022-10-13 17:57:12 -0600

    [Add] Index page product gallery

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Hero from '../components/Hero/Hero';
+import Gallery from '../components/Product/Gallery';
 
 export async function getServerSideProps(ctx) {
   const testProducts = [
@@ -6,16 +7,19 @@ export async function getServerSideProps(ctx) {
       id: 1,
       slug: 'blue-haze-1gr-indica',
       name: 'Gorila Glue',
+      price: 30000,
     },
     {
       id: 2,
       slug: 'blue-haze-1gr-indica',
       name: 'Blue Dream',
+      price: 85000,
     },
     {
       id: 3,
       slug: 'blue-haze-1gr-indica',
       name: 'Gelato',
+      price: 60000,
     },
   ];
 
@@ -26,6 +30,7 @@ export default function Home({ featuredStrains }) {
   return (
     <>
       <Hero strains={featuredStrains} />
+      <Gallery products={featuredStrains} />
     </>
   );
 }
