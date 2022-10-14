d842a23e123b928b52a9e982036246a4c7685b24
vladwithcode <vladwithcode@gmail.com>
2022-10-14 13:58:17 -0600

    [Change] Gallery scrolls only horizontally

--- src/components/Product/Gallery.jsx
+++ src/components/Product/Gallery.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 function Gallery({ products }) {
   return (
-    <div className='w-full flex flex-row items-center p-4 overflow-scroll gap-x-4'>
+    <div className='w-full flex flex-row items-center p-4 overflow-x-scroll gap-x-4'>
       {products?.map(product => (
         <ProductCard product={product} key={product.id} />
       ))}
