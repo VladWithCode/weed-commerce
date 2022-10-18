0fd1b4a40da656dbb6f0ba16f5254f482cb6a5c7
vladwithcode <vladwithcode@gmail.com>
2022-10-18 17:44:53 -0600

    [Add] Create product endpoint (mock)

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,3 +1,10 @@
+import {
+  createProduct,
+  writeProductFiles,
+} from '../../../services/ProductService';
+import asyncHandler from '../../../utils/asyncHandler';
+import parseFormData from '../../../utils/parseFormData';
+
 const handlers = {
   GET: async (req, res) => {
     const { lim } = req.query;
@@ -26,6 +33,32 @@ const handlers = {
     return res.status(200).json({
       products: testProducts,
     });
+  },
+
+  POST: async (req, res) => {
+    const { data, files } = await parseFormData(req);
+
+    const product = await createProduct(data);
+
+    const [writeError] = await asyncHandler(
+      writeProductFiles(files, product.assetPath)
+    );
+
+    if (writeError)
+      return res.status(201).json({
+        product,
+        warn: { message: 'Images could not be uploaded', error: writeError },
+      });
+
+    return res.status(201).json({
+      product,
+    });
+  },
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
   },
 };
 
