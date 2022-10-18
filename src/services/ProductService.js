a9a6250b93067d7a1ebeebbd09008ce72b612689
vladwithcode <vladwithcode@gmail.com>
2022-10-18 17:41:30 -0600

    [Change] Get, create product fns

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,9 +1,47 @@
-export const fetchProducts = async (lim = 0) => {
-  let url = '/api/products';
+import Product from '../models/Product';
+import asyncHandler from '../utils/asyncHandler';
+import { createDir, mvFile } from '../utils/fs';
+import { join } from 'path';
+import isEmptyObject from '../utils/isEmptyObject';
 
-  const response = await fetch(url);
+export const getProducts = async ({
+  query = {},
+  select,
+  lim = 0,
+  skip = 0,
+}) => {
+  let q = Product.find(query).limit(lim).skip(skip);
 
-  if (!response.ok) throw new Error('Network Error!');
+  if (!isEmptyObject(select)) q.select(select);
 
-  return response.json();
+  return await q;
 };
+
+export const createProduct = async data => {
+  const product = new Product(data);
+
+  if (!product.slug)
+    product.slug = product.name.toLowerCase().replace(/[\s,.]/g, '-');
+
+  product.assetPath = '/products/' + product.slug + '/';
+
+  return product;
+};
+
+export async function writeProductFiles(files, path) {
+  const [createDirError] = await asyncHandler(
+    createDir('public' + path, {
+      r: true,
+    })
+  );
+
+  if (createDirError) throw createDirError;
+
+  const writePromises = files.map(f => {
+    return mvFile(f.filepath, join('/public', path, f.originalFilename));
+  });
+
+  const [writeError] = await asyncHandler(Promise.all(writePromises));
+
+  if (writeError) throw writeError;
+}
