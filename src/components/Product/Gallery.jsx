217952d2ce5e3cdb9a5bbd00ca5ded2e41153931
vladwithcode <vladwithcode@gmail.com>
2022-10-19 17:37:10 -0600

    [Change] Gallery scrollbar styles

--- src/components/Product/Gallery.jsx
+++ src/components/Product/Gallery.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 function Gallery({ products }) {
   return (
-    <div className='w-full flex flex-row items-center p-4 overflow-x-scroll gap-x-4'>
+    <div className='w-full flex flex-row items-center p-4 overflow-x-auto gap-x-4 --scroll --scroll-x'>
       {products?.map(product => (
         <ProductCard product={product} key={product.id} />
       ))}
