f5f117a663d031df0fb00be995b5b12bf883b694
vladwithcode <vladwithcode@gmail.com>
2022-10-19 17:41:04 -0600

    [Change] use DB Products

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,37 +4,20 @@ import Hero from '../components/Hero/Hero';
 import Gallery from '../components/Product/Gallery';
 import { fetchProducts } from '../utils/fetchers/products';
 
-export async function getServerSideProps(ctx) {
-  const testProducts = [
-    {
-      id: 1,
-      slug: 'blue-haze-1gr-indica',
-      name: 'Gorila Glue',
-      price: 30000,
-    },
-    {
-      id: 2,
-      slug: 'blue-haze-1gr-indica',
-      name: 'Blue Dream',
-      price: 85000,
-    },
-    {
-      id: 3,
-      slug: 'blue-haze-1gr-indica',
-      name: 'Gelato',
-      price: 60000,
-    },
-  ];
-
-  return { props: { featuredStrains: testProducts } };
-}
-
-export default function Home({ featuredStrains }) {
+export default function Home({}) {
   const { error, isLoading, data } = useQuery(['products'], fetchProducts);
+
+  if (isLoading || error)
+    return (
+      <div className='flex flex-col justify-center items-center h-screen w-screen'>
+        <span className='text-white text-6xl font-bold py-12'>Loading...</span>
+        <span className='rounded-full border-transparent border-t-white border-8 animate-spin w-12 h-12'></span>
+      </div>
+    );
 
   return (
     <>
-      <Hero strains={featuredStrains} />
+      <Hero strains={data.products} />
       {!isLoading && data.products && <Gallery products={data.products} />}
       <FAQ />
     </>
