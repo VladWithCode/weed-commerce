e51573920766b888abdfddb0f0aa46f40aa22a5a
vladwithcode <vladwithcode@gmail.com>
2022-10-20 12:38:29 -0600

    [Change] Write actions inside of create

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,7 +1,7 @@
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
 
-const INITIAL_STATE = {
+export const CART_INITIAL_STATE = {
   items: [],
   count: 0,
   subtotal: 0,
@@ -12,92 +12,88 @@ const INITIAL_STATE = {
 export const useCart = create(
   persist(
     set => ({
-      ...INITIAL_STATE,
+      ...CART_INITIAL_STATE,
+
       setShipping: amount =>
         set(state => ({ shipping: amount, total: state.subtotal + amount })),
-      addItem: item => addItem(set, item),
-      removeItem: id => removeItem(set, id),
-      setItemQty: (id, qty) => setItemQty(set, id, qty),
+
+      addItem: item =>
+        set(state => {
+          let subtotal = 0;
+          let wasInCart = false;
+
+          const newItems = state.items.map(stateItem => {
+            let updatedItem = { ...stateItem };
+
+            if (stateItem.id === item.id) {
+              wasInCart = true;
+              updatedItem.qty = updatedItem.qty + 1;
+            }
+
+            subtotal += updatedItem.qty * updatedItem.price;
+            return updatedItem;
+          });
+
+          if (!wasInCart) {
+            newItems.push({
+              ...item,
+              subtotal: item.price,
+              qty: 1,
+            });
+
+            subtotal += item.price;
+          }
+
+          return {
+            items: newItems,
+            count: newItems.length,
+            subtotal,
+            total: subtotal + state.shipping,
+          };
+        }),
+
+      removeItem: id =>
+        set(state => {
+          let subtotal = 0;
+          const newItems = state.items.filter(stateItem => {
+            if (stateItem.id === id) return false;
+            else {
+              subtotal += stateItem.price * stateItem.qty;
+            }
+          });
+
+          return {
+            items: newItems,
+            count: newItems.length,
+            subtotal,
+            total: subtotal + state.shipping,
+          };
+        }),
+
+      setItemQty: (id, qty) =>
+        set(state => {
+          let subtotal = 0;
+          const newItems = state.items.map(stateItem => {
+            let updatedItem = { ...stateItem };
+            if (stateItem.id === id) {
+              updatedItem = {
+                ...updatedItem,
+                qty: qty,
+                subtotal: updatedItem.price * qty,
+              };
+            }
+
+            subtotal += updatedItem.subtotal;
+            return updatedItem;
+          });
+
+          return {
+            items: newItems,
+            subtotal,
+            total: subtotal + state.shipping,
+          };
+        }),
     }),
     { name: 'cart-storage' }
   )
 );
-
-const addItem = (set, itemToAdd) => {
-  set(state => {
-    let subtotal = 0;
-    let wasInCart = false;
-    const newItems = state.items.map(stateItem => {
-      let updatedItem = { ...stateItem };
-
-      if (stateItem.id === itemToAdd.id) {
-        wasInCart = true;
-        updatedItem.qty = updatedItem.qty + 1;
-      }
-
-      subtotal = updatedItem.qty * updatedItem.price;
-      return updatedItem;
-    });
-
-    if (!wasInCart) {
-      newItems.push({
-        ...itemToAdd,
-        subtotal: itemToAdd.price,
-        qty: 1,
-      });
-
-      subtotal += itemToAdd.price;
-    }
-
-    return {
-      items: newItems,
-      count: newItems.length,
-      subtotal,
-      total: subtotal + state.shipping,
-    };
-  });
-};
-
-const removeItem = (set, id) => {
-  set(state => {
-    let subtotal = 0;
-    const newItems = state.items.filter(stateItem => {
-      if (stateItem.id === id) return false;
-      else {
-        subtotal += stateItem.price * stateItem.qty;
-      }
-    });
-
-    return {
-      items: newItems,
-      count: newItems.length,
-      subtotal,
-      total: subtotal + state.shipping,
-    };
-  });
-};
-
-const setItemQty = (set, id, qty) => {
-  set(state => {
-    let subtotal = 0;
-    const newItems = state.items.map(stateItem => {
-      let updatedItem = { ...stateItem };
-      if (stateItem.id === id) {
-        updatedItem = {
-          ...updatedItem,
-          qty: qty,
-          subtotal: updatedItem.price * qty,
-        };
-      }
-
-      subtotal += updatedItem.subtotal;
-      return updatedItem;
-    });
-
-    return {
-      items: newItems,
-      subtotal,
-      total: subtotal + state.shipping,
-    };
-  });
-};
