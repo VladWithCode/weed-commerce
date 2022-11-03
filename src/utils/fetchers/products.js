1629d23aa072a3ab81b5e14db6ee0b65e808b0cf
vladwithcode <vladwithcode@gmail.com>
2022-11-03 10:29:23 -0700

    [Change] Use limit on fetchProducts

--- src/utils/fetchers/products.js
+++ src/utils/fetchers/products.js
@@ -1,7 +1,17 @@
-export const fetchProducts = async (lim = 0) => {
-  let url = '/api/products';
+export const fetchProducts = async ({ limit = 0 }) => {
+  let filters = {};
 
-  const response = await fetch(url);
+  if (limit > 0) filters.limit = limit;
+
+  let keys = Object.keys(filters);
+
+  const response = await fetch(
+    '/api/products'.concat(
+      keys.length > 0
+        ? '?' + keys.map((k, i) => `${i > 0 ? '&' : ''}${k}=${filters[k]}`)
+        : ''
+    )
+  );
 
   if (!response.ok) throw new Error('Network Error!');
 
