ab76d834d26627147b4dda816d884acc8b5ff2a6
vladwithcode <vladwithcode@gmail.com>
2022-11-03 10:39:04 -0700

    [Change] Use Loading component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,19 @@
 import { useQuery } from 'react-query';
 import FAQ from '../components/Faq/FAQ';
+import Loading from '../components/globals/Loading';
 import Hero from '../components/Hero/Hero';
 import Gallery from '../components/Product/Gallery';
 import { fetchProducts } from '../utils/fetchers/products';
 
 export default function Home({}) {
-  const { error, isLoading, data } = useQuery(['products'], fetchProducts);
+  const { error, isLoading, data } = useQuery(['products'], () =>
+    fetchProducts({ limit: 5 })
+  );
 
   if (isLoading || error)
     return (
-      <div className='flex flex-col justify-center items-center h-screen w-screen'>
-        <span className='text-white text-6xl font-bold py-12'>Loading...</span>
-        <span className='rounded-full border-transparent border-t-white border-8 animate-spin w-12 h-12'></span>
+      <div className='w-screen h-screen'>
+        <Loading />
       </div>
     );
 
