7132745dbd6c7a4780f427dfa53c5fdd2868fb66
vladwithcode <vladwithcode@gmail.com>
2022-11-03 17:44:17 -0700

    [Change] Put Logic on Service & Route handling on controller

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,84 +1,12 @@
 import {
   createProduct,
-  getProducts,
-  writeProductFiles,
+  getAllProducts,
 } from '../../../services/ProductService';
-import asyncHandler from '../../../utils/asyncHandler';
 import connectMongo from '../../../utils/db';
-import parseFormData from '../../../utils/parseFormData';
 
 const handlers = {
-  GET: async (req, res) => {
-    const { limit, skip } = req.query;
-
-    const [findError, products] = await asyncHandler(
-      getProducts({
-        select: {
-          name: 1,
-          slug: 1,
-          price: 1,
-          thumb: 1,
-          assetPath: 1,
-          stock: 1,
-        },
-        limit: parseInt(limit),
-        skip: parseInt(skip),
-        lean: true,
-      })
-    );
-
-    if (findError)
-      return res.status(500).json({
-        message: 'There was an error while retrieving the documents',
-        error: process.env.NODE_ENV !== 'production' ? findError : undefined,
-      });
-
-    return res.status(200).json({
-      products: products.map(p => ({ ...p, id: p._id, _id: undefined })),
-    });
-  },
-
-  POST: async (req, res) => {
-    const { data, files } = await parseFormData(req);
-
-    const [createError, product] = await asyncHandler(createProduct(data));
-
-    if (createError) {
-      return res.status(400).json({
-        message: 'Error while saving the product',
-        error: process.env.NODE_ENV !== 'production' ? createError : undefined,
-      });
-    }
-
-    const [writeError] = await asyncHandler(
-      writeProductFiles(files, product.assetPath)
-    );
-
-    if (writeError)
-      return res.status(201).json({
-        product,
-        warn: { message: 'Images could not be uploaded', error: writeError },
-      });
-
-    files.forEach(f => {
-      product.pics.push(f.originalFilename);
-    });
-
-    product.thumb = product.pics[0];
-
-    const [saveError] = await asyncHandler(product.save());
-
-    if (saveError) {
-      return res.status(400).json({
-        message: 'Error while saving the product',
-        error: process.env.NODE_ENV !== 'production' ? createError : undefined,
-      });
-    }
-
-    return res.status(201).json({
-      product,
-    });
-  },
+  GET: getAllProducts,
+  POST: createProduct,
 };
 
 export const config = {
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,36 +2,145 @@ import Product from '../models/Product';
 import asyncHandler from '../utils/asyncHandler';
 import { createDir, mvFile } from '../utils/fs';
 import { join } from 'path';
-import isEmptyObject from '../utils/isEmptyObject';
+import { requestErrorHandler } from '../utils/backend/requestErrorHandler';
+import parseFormData from '../utils/parseFormData';
 
-export const getProducts = async ({
-  query = {},
-  select,
-  limit = 0,
-  skip = 0,
-  lean = false,
-}) => {
-  let q = Product.find(query).limit(limit).skip(skip);
+export const getAllProducts = async (req, res) => {
+  let { limit, page } = req.query;
+  let skip = page * limit - limit;
 
-  if (!isEmptyObject(select)) q.select(select);
+  let q = Product.find({})
+    .select({
+      name: 1,
+      slug: 1,
+      price: 1,
+      thumb: 1,
+      assetPath: 1,
+      stock: 1,
+    })
+    .limit(parseInt(limit))
+    .skip(parseInt(skip))
+    .lean();
 
-  if (lean) q.lean();
+  const [findError, products] = await asyncHandler(q.exec());
 
-  return await q;
+  if (findError)
+    return requestErrorHandler(findError, {
+      customMessage: 'Error retriving the documents.',
+      req,
+      res,
+    });
+
+  return res.status(200).json({ result: 'OK', products });
 };
 
-export const createProduct = async data => {
+export const createProduct = async (req, res) => {
+  const { data, files } = await parseFormData(req);
   const product = new Product(data);
 
   if (!product.slug)
     product.slug = product.name.toLowerCase().replace(/[\s,.]/g, '-');
 
   product.assetPath = '/products/' + product.slug + '/';
+  product.absolutePath = join(__dirname, '../../public/products', product.slug);
 
-  product.ctgs = data.ctgs?.split(',') || 'sin categoria';
+  files.forEach((f, i) => {
+    if (i === 0) product.thumb = f.originalFilename;
+    product.pics.push(f.originalFilename);
+  });
 
-  return await product.save();
+  const [saveError, savedProduct] = await asyncHandler(product.save());
+
+  if (saveError)
+    return requestErrorHandler(saveError, {
+      customMessage: 'Error while saving the product.',
+      req,
+      res,
+    });
+
+  const [writeError] = await asyncHandler(
+    writeProductFiles(files, savedProduct.assetPath)
+  );
+
+  return res.json({
+    result: 'OK',
+    warn: writeError
+      ? { message: 'Images could not be uploade', error: writeError }
+      : undefined,
+    product: savedProduct,
+  });
 };
+
+export async function getProductById(req, res) {
+  const { id } = req.params;
+
+  const [findError, product] = await Product.findById(id)
+    .populate('category')
+    .lean();
+
+  if (findError)
+    return requestErrorHandler(findError, {
+      customMessage: 'Error while retrieving the product.',
+      req,
+      res,
+    });
+
+  return res.json({
+    result: 'OK',
+    product: { ...product.toJSON(), category: product.category.name },
+  });
+}
+
+export async function updateProductById(req, res) {
+  const { id } = req.query;
+  const data = req.body;
+
+  const [findError, product] = await asyncHandler(Product.findById(id));
+
+  if (findError) return requestErrorHandler(findError);
+
+  product.set(data);
+
+  const [saveError] = await asyncHandler(product.save());
+
+  if (saveError)
+    return requestErrorHandler(saveError, {
+      customMessage: 'Error while updating document.',
+      req,
+      res,
+    });
+
+  return res.json({
+    result: 'OK',
+    product,
+  });
+}
+
+export async function deleteProductById(req, res) {
+  const { id } = req.query;
+
+  const [deleteError] = await asyncHandler(Product.deleteOne({ _id: id }));
+
+  if (deleteError)
+    return requestErrorHandler(deleteError, {
+      req,
+      res,
+      customMessage: 'Error while deleting document.',
+    });
+
+  return res.json({ result: 'OK' });
+}
+
+export async function uploadFiles(req, res) {
+  const id = req.query.id;
+  const { files } = await parseFormData(req);
+
+  console.log(files);
+
+  return res.send('uwu');
+}
+
+/*-- Helpers --*/
 
 export async function writeProductFiles(files, path) {
   const [createDirError] = await asyncHandler(
