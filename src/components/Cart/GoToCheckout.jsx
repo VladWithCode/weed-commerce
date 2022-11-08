5c23ef3a54d54b05937a912558810650902d05d2
vladwithcode <vladwithcode@gmail.com>
2022-11-08 13:13:25 -0700

    [Add] Go To Checkout card

---
+++ src/components/Cart/GoToCheckout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { priceToString } from '../../utils/numberToString';
+import Button from '../globals/Button';
+
+function GoToCheckout({ count, subtotal }) {
+  return (
+    <div className='p-4 h-40 lg:h-60 flex flex-col text-xl border-2 border-indigo-500 border-opacity-25 rounded bg-zinc-800'>
+      <div className='flex justify-between mb-2'>
+        <div className='font-bold'>
+          <div>Subtotal:</div>
+          <div>({count} productos)</div>
+        </div>
+        <div className='font-light'>${priceToString(subtotal)}</div>
+      </div>
+      <Button className='mt-auto mb-4 ml-auto text-lg w-full'>
+        Proceder al Pago
+      </Button>
+    </div>
+  );
+}
+
+export default GoToCheckout;
