6ef78c4051a16c20d5b5e99df76b43172f55644a
vladwithcode <vladwithcode@gmail.com>
2022-11-12 11:00:17 -0700

    [Change] Test new positioning

--- src/components/Hero/Slide.jsx
+++ src/components/Hero/Slide.jsx
@@ -17,11 +17,11 @@ function Slide({ strain, slideId, currentSlide }) {
         className='row-span-full col-span-full z-10 object-cover object-center'
       />
       <span className='row-span-full col-span-full z-20 bg-gray-800 bg-opacity-40'></span>
-      <h1 className='row-start-2 col-start-1 col-span-2 mx-8 sm:col-span-1 sm:col-start-1 sm:justify-self-end z-30 text-white text-4xl uppercase font-bold'>
+      <h1 className='row-start-2 col-start-1 col-span-2 px-8 sm:mx-auto sm:col-span-1 sm:col-start-1 sm:justify-self-end z-30 text-white text-4xl uppercase font-bold'>
         {strain.name}
       </h1>
       <Link href={'/tienda/strains/'.concat(strain.slug)}>
-        <a className='btn bg-aux_1 p-2 row-start-3 col-start-2 justify-self-center self-start rounded-sm text-md text-white font-normal font-secondary z-30'>
+        <a className='btn bg-aux_1 p-2 row-start-3 col-start-1 justify-self-center self-start rounded-sm text-md text-white font-normal font-secondary z-30'>
           Compralo ahora
         </a>
       </Link>
