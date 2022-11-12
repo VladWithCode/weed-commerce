ba490a56b3a0502b79497ebad315ff3dcaed583f
vladwithcode <vladwithcode@gmail.com>
2022-11-12 13:13:19 -0700

    [Change] Product Listing

--- src/components/store/ProductListing.jsx
+++ src/components/store/ProductListing.jsx
@@ -1,43 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useShop } from '../../hooks/useShop';
-import CategoryRow from '../Product/CategoryRow';
+import React, { useState } from 'react';
+import ProductCard from '../Product/ProductCard';
 
-function ProductListing({ categories }) {
-  const categoryFilters = useShop(state => state.categories);
-  const [activeCategories, setActiveCategories] = useState(categories);
-
-  useEffect(() => {
-    const filtersActive = [];
-
-    for (let filter of categoryFilters) {
-      if (filter.active) filtersActive.push(filter.label);
-    }
-
-    if (filtersActive.length > 0) {
-      console.log(filtersActive);
-      setActiveCategories(
-        categories.filter(ctg => filtersActive.includes(ctg.name))
-      );
-    } else {
-      setActiveCategories(categories);
-    }
-  }, [categoryFilters]);
+function ProductListing({ products, page }) {
+  const [activeProducts, setActiveProducts] = useState(products);
 
   return (
-    <div id='products' className='col-start-3 col-span-6'>
-      {activeCategories.map(ctg => (
-        <CategoryRow
-          href={`/tienda/${ctg.name}`}
-          heading={ctg.name}
-          products={ctg.products}
-          key={ctg._id}
-        />
-      ))}
-      {activeCategories.length === 0 ? (
-        <p className='text-3xl text-center text-zinc-400 py-12'>
-          No se encontraron productos para los filtros seleccionados...
-        </p>
-      ) : null}
+    <div className='col-start-3 col-span-6'>
+      <div className='grid grid-cols-3 gap-y-6'>
+        {activeProducts.map(product => (
+          <ProductCard
+            product={product}
+            key={product.id}
+            className='w-80 mx-auto'
+          />
+        ))}
+      </div>
     </div>
   );
 }
