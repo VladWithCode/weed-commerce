f21e4c704b76f733f1cf05ef8578cdf8ad793445
vladwithcode <vladwithcode@gmail.com>
2022-11-12 10:58:32 -0700

    [Change] +Height +FntSize on bigger screens

--- src/components/globals/PageHeader.jsx
+++ src/components/globals/PageHeader.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 function PageHeader({ children, bgImg = '/img/placeholder.jpg' }) {
   return (
-    <div className='relative flex overflow-hidden items-center h-[25vh] pt-4 bg-black bg-opacity-40 text-white'>
+    <div className='relative flex overflow-hidden items-center h-[28vh] pt-4 bg-black bg-opacity-50 text-white'>
       <div className='absolute top-0 bottom-0 w-screen h-screen -z-10'>
         <Image src={bgImg} layout='fill' objectFit='cover' />
       </div>
-      <h1 className='text-2xl font-semibold px-6 z-10 container mx-auto'>
+      <h1 className='text-2xl 2xl:text-3xl font-semibold px-6 z-10 container mx-auto'>
         {children}
       </h1>
     </div>
