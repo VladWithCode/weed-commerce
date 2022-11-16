090b855f6838df7e54b52dfc5155161519917ccc
vladwithcode <vladwithcode@gmail.com>
2022-11-16 16:58:20 -0700

    [Add] Product fetcher by slug

--- src/utils/fetchers/products.js
+++ src/utils/fetchers/products.js
@@ -18,10 +18,7 @@ export const fetchProducts = async ({ limit = 0 }) => {
   return response.json();
 };
 
-export const fetchProductsPerCategory = async (
-  ctg,
-  { limit = 9, page = 1 }
-) => {
+export const fetchProductsByCategory = async (ctg, { limit = 9, page = 1 }) => {
   const response = await fetch(
     `/api/products/per-category/${ctg}?limit=${limit}&page=${page}`,
     {
@@ -33,6 +30,15 @@ export const fetchProductsPerCategory = async (
   const data = await response.json();
 
   if (!response.ok) throw new Error(data.message);
+
+  return data;
+};
+
+export const fetchProductBySlug = async slug => {
+  const response = await fetch(`/api/products/slug/${slug}`);
+  const data = await response.json();
+
+  if (!response.ok) throw new Error(data.message || 'Network Error');
 
   return data;
 };
