64c0109b348936c4fe685c037b1552ef391d74a0
vladwithcode <vladwithcode@gmail.com>
2022-11-22 11:38:40 -0700

    [Add] Remove and SetQty handles

--- src/components/Cart/EditableItem.jsx
+++ src/components/Cart/EditableItem.jsx
@@ -4,9 +4,14 @@ import Card from '../../components/globals/Card';
 import { useCart } from '../../hooks/useCart';
 import { priceToString } from '../../utils/numberToString';
 import QtySelector from './QtySelector';
+import shallow from 'zustand/shallow';
 
 function Item({ id, setQty }) {
   const items = useCart(state => state.items);
+  const { removeItem, setItemQty } = useCart(
+    state => ({ removeItem: state.removeItem, setItemQty: state.setItemQty }),
+    shallow
+  );
 
   const item = items.find(it => it.id === id);
 
@@ -14,7 +19,7 @@ function Item({ id, setQty }) {
 
   return (
     <Card
-      className='grid grid-cols-4 grid-rows-2 lg:flex lg:flex-row basis-52 border-indigo-500 border-b-2 border-opacity-10'
+      className='grid grid-cols-4 grid-rows-3 lg:flex lg:flex-row basis-52 border-indigo-500 border-b-2 border-opacity-10'
       replaceBaseClass={true}>
       <div className='col-start-1 col-span-2 row-span-2 self-center'>
         <Image
@@ -28,13 +33,23 @@ function Item({ id, setQty }) {
         <p className='text-xl overflow-hidden text-ellipsis mb-3'>
           {item.name}
         </p>
-        <QtySelector id={item.id} qty={item.qty} setQty={setQty} />
+        <QtySelector id={item.id} qty={item.qty} setQty={setItemQty} />
       </div>
-      <div className='grow-1 my-auto mt-2 ml-auto px-4'>
+      <div className='ml-auto self-center px-4 h-2/6'>
         <span className='text-gray-500 font-semibold pr-2'>
           {item.qty}&times;
         </span>
         <span className='text-lg'>${priceToString(item.price)}</span>
+      </div>
+
+      <div className='self-end mb-4 row-start-3 col-start-3 justify-self-center col-span-2'>
+        <button
+          className='text-white hover:text-red-600'
+          onClick={() => removeItem(id)}>
+          <svg className='w-4 h-4 fill-current'>
+            <use href='/svg/sprites.svg#bin'></use>
+          </svg>
+        </button>
       </div>
     </Card>
   );
