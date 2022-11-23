5ba139f7b91bc4d1c2f0dccd628f899521b78edb
vladwithcode <vladwithcode@gmail.com>
2022-11-22 19:00:10 -0700

    [Add] Shipping form

---
+++ src/components/forms/ShippingForm.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+function ShippingForm({ onSubmit, props }) {
+  const [fields, onInputChange] = useForm({
+    street: '',
+    num: '',
+    intNum: '',
+    hood: '',
+    state: '',
+    city: '',
+    zip: '',
+    refs: '',
+  });
+
+  const _onSubmit = e => {
+    e.preventDefault();
+
+    onSubmit(fields);
+  };
+
+  return (
+    <>
+      <p className='text-xl'>Direccion de envio</p>
+      <form className='py-6' onSubmit={_onSubmit} {...props}>
+        <div className='flex mb-4'>
+          <Input
+            label='Calle'
+            id='street'
+            name='street'
+            className='w-4/6 px-1'
+            onChange={onInputChange}
+            value={fields['street']}
+          />
+          <Input
+            id='num'
+            name='num'
+            label='Numero'
+            className='w-2/6 px-1'
+            onChange={onInputChange}
+            value={fields['num']}
+          />
+        </div>
+        <div className='flex mb-4'>
+          <Input
+            label='Colonia'
+            id='hood'
+            name='hood'
+            className='px-1 w-full'
+            onChange={onInputChange}
+            value={fields['hood']}
+          />
+        </div>
+        <div className='flex mb-4'>
+          <Input
+            label='Ciudad'
+            id='city'
+            name='city'
+            className='px-1 w-1/2'
+            onChange={onInputChange}
+            value={fields['city']}
+          />
+          <Input
+            label='Estado'
+            id='state'
+            name='state'
+            className='px-1 w-1/2'
+            onChange={onInputChange}
+            value={fields['state']}
+          />
+        </div>
+        <div className='flex mb-4'>
+          <Input
+            label='Codigo Postal'
+            id='zip'
+            name='zip'
+            className='px-1 w-full'
+            onChange={onInputChange}
+            value={fields['zip']}
+          />
+        </div>
+        <div className='flex mb-4'>
+          <Input
+            label='Referencias'
+            id='refs'
+            name='refs'
+            className='px-1 w-full'
+            onChange={onInputChange}
+            value={fields['refs']}
+          />
+        </div>
+        <div className='flex'>
+          <Button type='submit' className='w-full'>
+            Siguiente
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default ShippingForm;
