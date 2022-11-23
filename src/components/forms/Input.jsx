138945f11f8e4cb3fa7135033c65287bb11cae97
vladwithcode <vladwithcode@gmail.com>
2022-11-22 18:57:20 -0700

    [Change] Floating Label & handle value

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import getClassname from '../../utils/getClassname';
 
-function Input({ type = 'text', label, name, id }) {
+function Input({ className, type = 'text', label, name, id, ...props }) {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <div className='flex flex-col pt-1 pb-2 last:pb-0'>
-      <label htmlFor={id} className='pb-1'>
+    <div
+      className={getClassname(
+        'relative flex flex-col pt-1 pb-2 text-gray-400',
+        className
+      )}>
+      <label
+        htmlFor={id}
+        className={getClassname(
+          'px-2 absolute -translate-y-1/2 transition-top',
+          props.value?.length > 0 || isFocused
+            ? 'top-0 pb-4 text-xs text-white'
+            : ' pb-2 top-1/2 text-sm'
+        )}>
         {label || name}
       </label>
       <input
         type={type}
         name={name}
         id={id}
-        className='px-2 py-1 rounded text-zinc-800'
+        className={getClassname(
+          'px-2 py-1 bg-transparent border-b-2 border-current focus:text-white focus:border-opacity-100 focus:bg-gray-800 outline-none transition-[background]',
+          props.value?.length > 0 && 'text-white'
+        )}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...props}
       />
     </div>
   );
