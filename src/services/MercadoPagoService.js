9a5d5e14071de5bbb8eda4868c66c3145f49d9d5
vladwithcode <vladwithcode@gmail.com>
2022-11-25 13:27:40 -0700

    [Change] Finished mercadopago createSale

--- src/services/MercadoPagoService.js
+++ src/services/MercadoPagoService.js
@@ -1,7 +1,12 @@
 import Product from '../models/Product';
+import Category from '../models/Category';
 import Sale from '../models/Sale';
 import asyncHandler from '../utils/asyncHandler';
 import { requestErrorHandler } from '../utils/backend/requestErrorHandler';
+
+const mercadopago = require('mercadopago');
+
+mercadopago.configurations.setAccessToken(process.env.MERCADOPAGO_ACCESS_TOKEN);
 
 export const createSale = async (req, res) => {
   const { mpData, saleData } = req.body;
@@ -9,12 +14,25 @@ export const createSale = async (req, res) => {
   const hashedQtys = {};
 
   const [findError, products] = await asyncHandler(
-    Product.find({
-      _id: saleData.items.map(i => {
-        hashedQtys[String(i._id)] = i.qty;
+    Product.find(
+      {
+        _id: saleData.items.map(item => {
+          hashedQtys[item.id] = item.qty;
 
-        return i._id;
-      }),
+          return item.id;
+        }),
+      },
+      {
+        name: 1,
+        description: 1,
+        category: 1,
+        price: 1,
+        thumb: 1,
+        assetPath: 1,
+      }
+    ).populate({
+      path: 'category',
+      transform: doc => doc.name,
     })
   );
 
@@ -25,6 +43,8 @@ export const createSale = async (req, res) => {
       customMessage: 'Error finding the products.',
     });
 
+  const { customer, shipping } = saleData;
+
   const paymentPayload = {
     ...mpData,
     additional_info: {
@@ -32,29 +52,28 @@ export const createSale = async (req, res) => {
         id: String(i._id),
         title: i.name,
         description: i.description,
-        picture_url: i.pics[0],
+        picture_url: i.thumb,
         category_id: i.category,
         quantity: hashedQtys[String(i._id)],
         unit_price: i.price / 100,
       })),
       payer: {
-        first_name: saleData.name,
-        last_name: saleData.lastname,
-        phone: { number: saleData.phone },
+        first_name: customer.names,
+        last_name: customer.lastname,
+        phone: { number: customer.phone },
         address: {
-          zip_code: saleData.zip,
-          street_name: saleData.street,
-          street_number: parseInt(saleData.extNumber),
+          zip_code: shipping.zip,
+          street_name: shipping.street,
+          street_number: parseInt(shipping.num),
         },
       },
       shipments: {
         receiver_address: {
-          zip_code: saleData.zip,
-          state_name: saleData.state,
-          city_name: saleData.city,
-          street_name: saleData.street,
-          street_number: parseInt(saleData.extNumber),
-          apartment: saleData.intNumber,
+          zip_code: shipping.zip,
+          state_name: shipping.state,
+          city_name: shipping.city,
+          street_name: shipping.street,
+          street_number: parseInt(shipping.num),
         },
       },
     },
@@ -65,27 +84,39 @@ export const createSale = async (req, res) => {
     .then(async response => {
       const data = response.body;
 
-      const newSale = new Sale({
-        userData: {
-          name: saleData.name + ' ' + saleData.lastname,
-          phone: saleData.phone,
-          email: saleData.email,
-        },
-        street: saleData.street,
-        col: saleData.col,
-        zip: saleData.zip,
-        num: saleData.extNumber,
-        intNum: saleData.intNumber,
-        refs: saleData.refs,
-        city: saleData.city,
-        state: saleData.state,
-        // country: saleData.county,
-        items: products.map(p => ({
+      const items = [];
+      let subtotal = 0;
+
+      for (let p of products) {
+        subtotal += p.price * hashedQtys[String(p._id)];
+
+        items.push({
           product: p._id,
           qty: hashedQtys[String(p._id)],
           total: p.price * hashedQtys[String(p._id)],
-        })),
-        paid: data.status === 'approved',
+        });
+      }
+
+      const newSale = new Sale({
+        userData: {
+          fullname: customer.names + ' ' + customer.lastname,
+          phone: customer.phone,
+          email: customer.email,
+        },
+        street: shipping.street,
+        col: shipping.hood,
+        zip: shipping.zip,
+        num: shipping.num,
+        // intNum: shipping.intNumber,
+        refs: shipping.refs,
+        city: shipping.city,
+        state: shipping.state,
+        // country: shipping.county,
+        items: items,
+        subtotal: subtotal,
+        shippingFee: saleData.shippingFee,
+        total: subtotal + (parseInt(saleData.shippingFee) || 0),
+        paidatad: data.status === 'approved',
         paidAt: data.date_approved,
         earnings: data.transaction_details.net_received_amount * 100,
         paymentData: {
@@ -100,7 +131,7 @@ export const createSale = async (req, res) => {
         let errorMessage =
           data.status === 'approved'
             ? 'Ocurrio un error al guardar tu compra en la base de datos. Por favor, contacta a soporte para solucionar este problema. ID de la venta: ' +
-              savedSale.id
+              data.id
             : 'La venta no se ha ejecutado correctamente';
 
         return requestErrorHandler(saveError, {
@@ -112,14 +143,7 @@ export const createSale = async (req, res) => {
 
       return res.status(response.status).json({
         result: 'OK',
-        sale: {
-          id: savedSale._id,
-          name: savedSale.userData.fullname,
-          items: savedSale.items,
-          subtotal: savedSale.subtotal,
-          shipping: savedSale.shipping,
-          total: savedSale.total,
-        },
+        saleId: savedSale._id,
       });
     })
     .catch(err => requestErrorHandler(err, { req, res }));
